3e4502d04d0cc5cb4ee5d67f638dcff592c4f5ed
Crossle Song <crosslesong@gmail.com>
2021-07-18 23:13:50 +0400

    Update tx

--- mobile/transaction.go
+++ mobile/transaction.go
@@ -1,52 +1,140 @@
 package mobile
 
 import (
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/MixinNetwork/mixin/common"
 	"github.com/MixinNetwork/mixin/crypto"
 )
 
+var httpClient *http.Client
+
+func callRPC(node, method string, params []interface{}) ([]byte, error) {
+	if httpClient == nil {
+		httpClient = &http.Client{Timeout: 30 * time.Second}
+	}
+
+	body, err := json.Marshal(map[string]interface{}{
+		"method": method,
+		"params": params,
+	})
+	if err != nil {
+		panic(err)
+	}
+	req, err := http.NewRequest("POST", node, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Close = true
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result struct {
+		Data  interface{} `json:"data"`
+		Error interface{} `json:"error"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	if result.Error != nil {
+		return nil, fmt.Errorf("ERROR %s", result.Error)
+	}
+
+	return json.Marshal(result.Data)
+}
+
 type signerInput struct {
 	Inputs []struct {
-		Hash  crypto.Hash   `json:"hash"`
-		Index int           `json:"index"`
-		Keys  []*crypto.Key `json:"keys"`
-		Mask  crypto.Key    `json:"mask"`
+		Hash    crypto.Hash         `json:"hash"`
+		Index   int                 `json:"index"`
+		Deposit *common.DepositData `json:"deposit,omitempty"`
+		Keys    []*crypto.Key       `json:"keys"`
+		Mask    crypto.Key          `json:"mask"`
 	} `json:"inputs"`
 	Outputs []struct {
 		Type     uint8             `json:"type"`
-		Mask     crypto.Key        `json:"mask"`
-		Keys     []*crypto.Key     `json:"keys"`
-		Amount   common.Integer    `json:"amount"`
 		Script   common.Script     `json:"script"`
-		Accounts []*common.Address `json:"accounts"`
+		Accounts []*common.Address `json:"accounts,omitempty"`
+		Keys     []*crypto.Key     `json:"keys,omitempty"`
+		Mask     crypto.Key        `json:"mask,omitempty"`
+		Amount   common.Integer    `json:"amount"`
 	}
 	Asset crypto.Hash `json:"asset"`
 	Extra string      `json:"extra"`
 	Node  string      `json:"-"`
 }
 
-func CreateTransaction(rawStr string) (string, error) {
-	var raw signerInput
-	err := json.Unmarshal([]byte(rawStr), &raw)
-	if err != nil {
-		return "", err
+func (raw signerInput) ReadUTXOKeys(hash crypto.Hash, index int) (*common.UTXOKeys, error) {
+	utxo := &common.UTXOKeys{}
+
+	for _, in := range raw.Inputs {
+		if in.Hash == hash && in.Index == index && len(in.Keys) > 0 {
+			utxo.Keys = in.Keys
+			utxo.Mask = in.Mask
+			return utxo, nil
+		}
 	}
 
+	data, err := callRPC(raw.Node, "getutxo", []interface{}{hash.String(), index})
+	if err != nil {
+		return nil, err
+	}
+	var out common.UTXOWithLock
+	err = json.Unmarshal(data, &out)
+	if err != nil {
+		return nil, err
+	}
+	if out.Amount.Sign() == 0 {
+		return nil, fmt.Errorf("invalid input %s#%d", hash.String(), index)
+	}
+	utxo.Keys = out.Keys
+	utxo.Mask = out.Mask
+	return utxo, nil
+}
+
+func (raw signerInput) CheckDepositInput(deposit *common.DepositData, tx crypto.Hash) error {
+	return nil
+}
+
+func (raw signerInput) ReadLastMintDistribution(group string) (*common.MintDistribution, error) {
+	return nil, nil
+}
+
+func SignTransaction(account common.Address, raw signerInput) (*common.SignedTransaction, error) {
 	tx := common.NewTransaction(raw.Asset)
 	for _, in := range raw.Inputs {
-		tx.AddInput(in.Hash, in.Index)
+		if in.Deposit != nil {
+			tx.AddDepositInput(in.Deposit)
+		} else {
+			tx.AddInput(in.Hash, in.Index)
+		}
 	}
 
 	for _, out := range raw.Outputs {
-		if out.Mask.HasValue() {
+		if out.Type != common.OutputTypeScript {
+			return nil, fmt.Errorf("invalid output type %d", out.Type)
+		}
+
+		if out.Accounts != nil {
+			tx.AddRandomScriptOutput(out.Accounts, out.Script, out.Amount)
+		}
+		if out.Keys != nil {
 			tx.Outputs = append(tx.Outputs, &common.Output{
-				Type:   out.Type,
+				Type:   common.OutputTypeScript,
 				Amount: out.Amount,
 				Keys:   out.Keys,
-				Script: out.Script,
+				Script: common.NewThresholdScript(1),
 				Mask:   out.Mask,
 			})
 		}
@@ -54,11 +142,46 @@ func CreateTransaction(rawStr string) (string, error) {
 
 	extra, err := hex.DecodeString(raw.Extra)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	tx.Extra = extra
 
-	signed := tx.AsLatestVersion()
-	msg := signed.AsLatestVersion().PayloadMarshal()
-	return hex.EncodeToString(msg), nil
+	signed := &common.SignedTransaction{Transaction: *tx}
+	for i := range signed.Inputs {
+		err := signed.SignInput(raw, i, []*common.Address{&account})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return signed, nil
+}
+
+func SignTransactionRaw(node string, account common.Address, rawStr string) (string, error) {
+	var raw signerInput
+	err := json.Unmarshal([]byte(rawStr), &raw)
+	if err != nil {
+		return "", err
+	}
+	raw.Node = node
+	tx, err := SignTransaction(account, raw)
+	if err != nil {
+		return "", err
+	}
+	d := &common.VersionedTransaction{SignedTransaction: *tx}
+	return hex.EncodeToString(d.Marshal()), nil
+}
+
+func SentRawTransaction(node string, raw string) error {
+	data, err := callRPC(node, "sendrawtransaction", []interface{}{raw})
+	if err != nil {
+		return err
+	}
+	var resp struct {
+		Hash string `json:"hash"`
+	}
+	err = json.Unmarshal(data, &resp)
+	if err != nil {
+		return err
+	}
+	return nil
 }
