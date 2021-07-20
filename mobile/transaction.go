614bf9805ebf88609c92afe6c1e01438f1569c65
Crossle Song <crosslesong@gmail.com>
2021-07-20 22:45:40 +0400

    cleanup

--- mobile/transaction.go
+++ mobile/transaction.go
@@ -162,33 +162,6 @@ func Sign(msg []byte, privateViewKey, privateSpendKey, mask *crypto.Key, index u
 	return sig
 }
 
-/*
-func signInput(signed *common.SignedTransaction, reader common.UTXOKeysReader, index int) (*common.SignedTransaction, error) {
-	msg := signed.AsLatestVersion().PayloadMarshal()
-
-	if index >= len(signed.Inputs) {
-		return nil, fmt.Errorf("invalid input index %d/%d", index, len(signed.Inputs))
-	}
-	in := signed.Inputs[index]
-	utxo, err := reader.ReadUTXOKeys(in.Hash, in.Index)
-	if err != nil {
-		return nil, err
-	}
-	if utxo == nil {
-		return nil, fmt.Errorf("input not found %s:%d", in.Hash.String(), in.Index)
-	}
-	if len(utxo.Keys) != 1 {
-		return nil, fmt.Errorf("utxo keys found %d", len(utxo.Keys))
-	}
-	sigs := make(map[uint16]*crypto.Signature)
-	fmt.Println(msg)
-	priv := crypto.DeriveGhostPrivateKey(&utxo.Mask, &acc.PrivateViewKey, &acc.PrivateSpendKey, uint64(in.Index))
-	sig := priv.Sign(msg)
-	sigs[0] = &sig
-	signed.SignaturesMap = append(signed.SignaturesMap, sigs)
-	return signed, nil
-}
-*/
 func createTx(raw signerInput) (string, error) {
 	tx := common.NewTransaction(raw.Asset)
 	for _, in := range raw.Inputs {
